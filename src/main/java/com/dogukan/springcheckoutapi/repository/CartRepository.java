86e8abb971445ee2f9f96aec0934e75c89164a5b
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-08-24 15:52:47 +0300

    feat: Added CartRepository interface

---
+++ src/main/java/com/dogukan/springcheckoutapi/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package com.dogukan.springcheckoutapi.repository;
+
+import com.dogukan.springcheckoutapi.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+}
