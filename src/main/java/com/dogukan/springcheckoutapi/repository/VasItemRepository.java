9354ce5c649f178df134098f828419bf91e54f10
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-08-24 15:56:43 +0300

    feat: Added VasItemRepository interface

---
+++ src/main/java/com/dogukan/springcheckoutapi/repository/VasItemRepository.java
@@ -0,0 +1,8 @@
+package com.dogukan.springcheckoutapi.repository;
+
+import com.dogukan.springcheckoutapi.entity.VasItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VasItemRepository extends JpaRepository<VasItem, Integer> {
+
+}
