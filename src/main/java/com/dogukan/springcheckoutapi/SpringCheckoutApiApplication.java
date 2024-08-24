7e7ffb0d0c52a596e6ef2054f430254740a30f42
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-08-24 14:56:19 +0300

    Initial commit

---
+++ src/main/java/com/dogukan/springcheckoutapi/SpringCheckoutApiApplication.java
@@ -0,0 +1,13 @@
+package com.dogukan.springcheckoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCheckoutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCheckoutApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dogukan/springcheckoutapi/SpringCheckoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukan.springcheckoutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCheckoutApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
